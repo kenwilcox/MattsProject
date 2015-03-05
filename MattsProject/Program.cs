a78e942301967a3d52d6b7a0f62d3ec208c9c144
Ken Wilcox <wilcoxk@gmail.com>
2015-03-05 15:52:59 -0700

    OK, now I'm starting to get silly

--- MattsProject/Program.cs
+++ MattsProject/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using MattsProject.Implementation;
+using MattsProject.Interface;
+using Microsoft.Practices.Unity;
 
 namespace MattsProject
 {
@@ -36,8 +40,39 @@ namespace MattsProject
       randomSquare.PrintNeighbors();
 
 
-      Board board = new Board(new TickTackToeBoard(), new BoardStyleHugsAndKisses());
-      board.ShowBoard();
+      //Board board = new Board(new TickTackToeBoard(), new BoardStyleHugsAndKisses());
+      //board.ShowBoard();
+
+      // Now some fun - Using a random board and random board style draw some output
+      // BUT we're going to be using unity for some DI goodness!
+      Assembly me = Assembly.GetExecutingAssembly();
+      List<Type> boards = new List<Type>();
+      List<Type> styles = new List<Type>();
+
+      foreach (Type type in me.GetTypes())
+      {
+        if (type.GetInterfaces().Contains(typeof (IBoardInterface)))
+        {
+          boards.Add(type);
+        }
+        else if (type.GetInterfaces().Contains(typeof (IBoardStyle)))
+        {
+          styles.Add(type);
+        }
+      }
+
+      Console.WriteLine();
+      Console.WriteLine(String.Format("Boards: {0} - Styles: {1}", boards.Count(), styles.Count()));
+
+      Type randomBoard = boards.OrderBy(x => Guid.NewGuid()).First();
+      Type randomStyle = styles.OrderBy(x => Guid.NewGuid()).First();
+
+      var container = new UnityContainer();
+      container.RegisterType(typeof (IBoardInterface), randomBoard);
+      container.RegisterType(typeof (IBoardStyle), randomStyle);
+
+      var obj = container.Resolve<Board>();
+      obj.ShowBoard();
 
       Console.ReadKey();
     }
